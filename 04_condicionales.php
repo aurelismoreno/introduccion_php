e3d4b912284953f524c5da8e154010013f46f9cb
aurelismoreno <aurelis.moreno@gmail.com>
2022-05-24 11:12:55 -0500

    variables

---
+++ 04_condicionales.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aprendiendo PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+  </head>
+  <body>
+    
+    <div class="container">
+          <h1>Aprendiendo PHP</h1>
+          
+          <div class="codigo">
+            <?php
+                $numero1=20;
+                $numero2=19;
+
+                if($numero1>$numero2){
+                    echo"el numero 1 es mayor";
+                } else {
+                    echo"el numero 2 es mayor";
+                }
+
+
+
+            ?>
+              
+          </div>
+    </div>
+    
+  </body>
+</html>
